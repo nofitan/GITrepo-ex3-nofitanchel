26200d900d5af4b86e600af3c2387418a2e6a4d4
nofitan <nofitmain@gmail.com>
2024-12-08 20:16:20 +0200

    fourth commit

--- nofitanchel.java
+++ nofitanchel.java
@@ -1,7 +1,7 @@
 public class nofitanchel{
 	public static void main(String[] args){
 		for(int i=1;i<=10;i++){
-			System.out.println("Nofit Anchel"+i);
+			System.out.println("Nofit Anchel second print"+i);
 		}
 	}
 }
